0aff1f7f72555c318486a267df7db7048ee34e59
gfumagali <guilhermefumarques@gmail.com>
2024-04-20 16:01:29 -0300

    feat: improving style and GET when parameter change

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import './App.css'
-import {useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-
+import {SelectButton} from "primereact/selectbutton";
+import {InputNumber} from "primereact/inputnumber";
+import {MappingMachineProblemCode} from "./constants/MappingMachineProblemCode.ts";
+import {Card} from "primereact/card";
 
 function App() {
 
   const [loading, setLoading] = useState(false)
+
+  const [machineDescription, setMachineDescription] = useState(
+    {
+      type: null,
+      air_temperature: null,
+      process_temperature: null,
+      rotational_speed: null,
+      torque: null,
+      tool_wear: null
+    }
+  )
+
   const [predict, setPredict] = useState({
     "Sem falha": 0,
     "Falha de energia": 0,
@@ -17,90 +30,123 @@ function App() {
     "Falha de dissipação de calor": 0,
   })
 
-  const MAP_RESPONSE = {
-    0: "Sem falha",
-    1: "Falha de energia",
-    2: "Falha de desgaste da ferramenta",
-    3: "Falha por excesso de trabalho",
-    4: "Falhas diversas",
-    5: "Falha de dissipação de calor",
-  }
+  const completedForm = useMemo(() => {
+    return Object.values(machineDescription).every(value => value !== null)
+  }, [machineDescription])
 
-  const handleSubmit = async (event) => {
-    event.preventDefault()
+  useEffect(() => {
 
-    const formData = new FormData(event.target)
-    const data = {
-      type: formData.get('type').toString(),
-      air_temperature: parseFloat(formData.get('air_temperature').toString()),
-      process_temperature: parseFloat(formData.get('process_temperature').toString()),
-      rotational_speed: parseFloat(formData.get('rotational_speed').toString()),
-      torque: parseFloat(formData.get('torque').toString()),
-      tool_wear: parseFloat(formData.get('tool_wear').toString()),
-      target: 1.0,
+    const getPredict = async (data) => {
+        return await fetch('http://127.0.0.1:8000/predict', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({...data, target: 1.0})
+        })
     }
 
-    setLoading(true)
+    if (completedForm) {
+        setLoading(true)
+        getPredict(machineDescription).then((response) => {
+            setLoading(false)
 
-    const response = await fetch('http://127.0.0.1:8000/predict', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    })
-
-    setLoading(false)
-
-    if (response.ok) {
-        const result = await response.json()
-        const predict = result[0].reduce((acc, value, index) => {
-            acc[MAP_RESPONSE[index]] = value
-            return acc
-        }, {})
-        setPredict(predict)
+            if (response.ok) {
+                response.json().then((result) => {
+                    const predict = result[0].reduce((acc, value, index) => {
+                        acc[MappingMachineProblemCode[index]] = value
+                        return acc
+                    }, {})
+                    setPredict(predict)
+                })
+            }
+        })
     }
-  }
+  }, [machineDescription]);
 
-  console.log(predict)
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Qualidade da máquina:
-          <select name="type">
-            <option value="L">Baixa</option>
-            <option value="M">Média</option>
-            <option value="H">Alta</option>
-          </select>
-        </label>
-        <label>
-          Temperatura do ar (°C):
-          <input type="number" name="air_temperature" min={23.15} max={32.35} step={0.5} />
-        </label>
-        <label>
-          Temperatura do processo (°C):
-          <input type="number" name="process_temperature" min={33.5} max={41.65} step={0.5} />
-        </label>
-        <label>
-          Velocidade de rotação (rpm):
-          <input type="number" name="rotational_speed" min={1168} max={2886} step={1} />
-        </label>
-        <label>
-          Torque (Nm):
-          <input type="number" name="torque" min={3.8} max={76.6} step={0.1} />
-        </label>
-        <label>
-          Desgaste da ferramenta (min):
-          <input type="number" name="tool_wear" min={0} max={253} step={1} />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
+    <Card title={"Predição de falhas em máquinas de fresagem"} style={{display: "flex", flexDirection: "row", gap: "1rem"}}>
+        <form title={"Parâmetros da máquina"} style={{display: "flex", flexDirection: "column", gap: "1rem"}}>
+            <label>
+                Qualidade da máquina:
+                <SelectButton
+                    value={machineDescription.type}
+                    onChange={(e) => setMachineDescription({...machineDescription, type: e.value})}
+                    optionLabel="label"
+                    options={[
+                        {label: 'Baixa', value: 'L'},
+                        {label: 'Média', value: 'M'},
+                        {label: 'Alta', value: 'H'}
+                    ]}/>
+            </label>
+            <label>
+                Temperatura do ar (°C):
+                <InputNumber
+                    value={machineDescription.air_temperature}
+                    onValueChange={(e) => setMachineDescription({...machineDescription, air_temperature: e.value})}
+                    mode={"decimal"}
+                    min={23.15}
+                    max={32.35}
+                    step={0.5}
+                    showButtons
+                />
+            </label>
+            <label>
+                Temperatura do processo (°C):
+                <InputNumber
+                    value={machineDescription.process_temperature}
+                    onValueChange={(e) => setMachineDescription({...machineDescription, process_temperature: e.value})}
+                    mode={"decimal"}
+                    min={33.5}
+                    max={41.65}
+                    step={0.5}
+                    showButtons
+                />
+            </label>
+            <label>
+                Velocidade de rotação (rpm):
+                <InputNumber
+                    value={machineDescription.rotational_speed}
+                    onValueChange={(e) => setMachineDescription({...machineDescription, rotational_speed: e.value})}
+                    mode={"decimal"}
+                    min={1168}
+                    max={2886}
+                    step={1}
+                    showButtons
+                />
+            </label>
+            <label>
+                Torque (Nm):
+                <InputNumber
+                    value={machineDescription.torque}
+                    onValueChange={(e) => setMachineDescription({...machineDescription, torque: e.value})}
+                    mode={"decimal"}
+                    min={3.8}
+                    max={76.6}
+                    step={0.1}
+                    showButtons
+                />
+            </label>
+            <label>
+                Desgaste da ferramenta (min):
+                <InputNumber
+                    value={machineDescription.tool_wear}
+                    onValueChange={(e) => setMachineDescription({...machineDescription, tool_wear: e.value})}
+                    mode={"decimal"}
+                    min={0}
+                    max={253}
+                    step={1}
+                    showButtons
+                />
+            </label>
+        </form>
+        <div>
+            <div>{!loading && predict && <p>{JSON.stringify(predict)}</p>}</div>
+            <div>Chart</div>
+        </div>
 
-      {loading && <p>Carregando...</p>}
-      {!loading && predict && <p>{JSON.stringify(predict)}</p>}
-    </>
+    </Card>
   )
 }
 
